7cd1ddb50f25e92faa8c37adbd4b4029ac400f21
scartlex <nmattela@infogroep.be>
2020-02-29 13:41:47 +0100

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import { SyncComponent } from '@nmattela/akko';
+
+class App extends SyncComponent {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                You're good to go!
+            </div>
+        );
+    }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from './Components/App/App.js';
+import { render } from '@nmattela/akko';
+
+const root = document.getElementById('root');
+render(root, new App());
