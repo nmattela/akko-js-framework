5a91435df80f401872db095b742a74ff9bd508a3
nmattela <nicolas.mattelaer@gmail.com>
2020-03-15 18:33:50 +0100

    Added public folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const resolve = require('path').resolve;
 
 module.exports = {
@@ -31,6 +32,9 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/index.html",
             filename: "./index.html"
-        })
+        }),
+        new CopyWebpackPlugin([
+            {from: './public', to: '.'}
+        ])
     ]
 };
