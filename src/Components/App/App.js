b8b37a59c2362c1ed28da16d6b079199b1e22d10
nmattela <nicolas.mattelaer@gmail.com>
2020-04-15 09:58:14 +0200

    Revert "[WIP] Initial commit for web workers"
    This reverts commit e5733e9848132018d4decfcd420f40039297abea.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,12 @@
 import { SyncComponent } from 'akko-js';
-import AsyncTest from "../AsyncTest/AsyncTest";
 
 class App extends SyncComponent {
     constructor(props) {
         super(props);
-
-        this.initState({
-            counter: 0
-        });
     }
 
     render() {
-        return (
-            <div>
-                <button onClick={() => this.state.counter++} >Go up!</button>
-                <h1>App: {this.state.counter}</h1>
-                <AsyncTest counter={this.state.counter} />
-            </div>
-        )
+        return <h1>You're good to go!</h1>
     }
 }
 
